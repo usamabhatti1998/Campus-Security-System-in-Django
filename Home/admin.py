9b26856dbe90d9b33a68f4dbf1b12b57d175d15f
Usama Bhatti <47285459+usamabhatti1998@users.noreply.github.com>
2022-02-19 15:40:28 +0500

    Add files via upload

---
+++ Home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import CSFR
+
+admin.site.register(CSFR)
+
+admin.site.site_header ='University of Lahore Campus Security System'
